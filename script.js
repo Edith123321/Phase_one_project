a3d3b5817a5ab97f36dab26efd09ad2f787b78d3
edith.githinji <edithgithinji2020@gmail.com>
2024-12-20 14:03:17 +0300

    declaring variables

---
+++ script.js
@@ -0,0 +1,9 @@
+// Element selectors
+const iconCart = document.querySelector('.icon-cart');
+const closeCart = document.querySelector('.close');
+const body = document.querySelector('body');
+const listProductHTML = document.querySelector('.listProduct');
+const listCartHTML = document.querySelector('.listCart');
+let iconCartSpan = document.querySelector('.icon-cart span');
+
+
